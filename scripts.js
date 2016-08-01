c1f6f204a08c13652c01d285ebf42e222cff4775
makiahamilton <princesskiyoo@gmail.com>
2016-08-01 11:30:42 -0700

    Added website files

---
+++ scripts.js
@@ -0,0 +1,54 @@
+document.onload = function(){
+     alert('hi!ds lknskN:SN:Kdvsn')
+
+}
+
+
+
+
+<!DOCTYPE html>
+    <html>
+
+
+    <head>
+        <title>My Page</title>
+        <!-- <link rel="stylesheet" href="style.css"> -->
+        <script src="style.css"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    </head>
+    <body><style>
+
+
+
+
+    !DOCTYPE html>
+    <html>
+    <link type="text/css" rel="stylesheet" href="style.css"/>
+
+
+    <head>
+        <title>My Page</title>
+        <!-- <link rel="stylesheet" href="style.css"> -->
+        <script src="style.css"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    </head>
+    <body><style>
+            
+
+
+
+        <center><img src="https://scontent-sjc2-1.xx.fbcdn.net/v/t34.0-12/13672459_870333946406136_224958373_n.jpg?oh=8d6e7b3e93b9ca04df316136fe81fc03&oe=579C341B"</center>
+
+
+           <head>
+        <title>My Page</title>
+        <!-- <link rel="stylesheet" href="style.css"> -->
+        <script src="style.css"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    </head>
+    <body><style>
+            
+
+
+
+        <center><img src="https://scontent-sjc2-1.xx.fbcdn.net/v/t34.0-12/13672459_870333946406136_224958373_n.jpg?oh=8d6e7b3e93b9ca04df316136fe81fc03&oe=579C341B"</center>
